8b15888c58587aa7a86e1d6726c6ba9914eb70f0
Kevin Johnston and Mark Lawson <mark@>
2016-08-09 15:39:59 -0700

    start adding a button to add new terms

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ $(function() {
   $(".card").click(function() {
     $("p").toggle();
     $("ul").toggle();
-
   });
 
   var counter = 1;
   var current = "card" + counter;
 
 // Next Button
+  var newCount = 20
   $("#next").click(function() {
     $("#" + current).addClass("hidden");
-    if (counter === 19) {
+    if (counter === newCount-1) {
       counter = 1;
     } else {
       counter += 1;
@@ -26,7 +26,7 @@ $(function() {
   $("#prev").click(function() {
     $("#" + current).addClass("hidden");
     if (counter === 1) {
-      counter = 19;
+      counter = newCount-1;
     } else {
       counter -= 1;
     }
@@ -44,6 +44,19 @@ $(function() {
     $("p").hide();
     $("ul").hide();
     $("#" + current).removeClass("hidden");
+  });
+
+// Add Term Button
+  $("#addTerm").click(function() {
+    var term = prompt("What is your new term?");
+    var definition = prompt("And its definition?");
+    $("#termListItems").append('<li class="term" value="' + newCount + '">' + term + '</li>');
+    $("#buttons").before('<div class="card hidden" id="card' + newCount + '"><h2>' + term + '</h2><p>' + definition + '</p></div>');
+    newCount += 1;
+    $(".card").click(function() {
+      $("p").toggle();
+      $("ul").toggle();
+    });
   });
 
 // Term List
