0d1eeb94412a989414bd37b88df763d4fcfe3db1
Kevin Johnston and Mark Lawson <mark@>
2016-08-09 10:34:19 -0700

    Add functionality to previous and next buttons

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,20 @@ $(function() {
   var counter = 1;
   var current = "card" + counter;
 
+// Next Button
   $("#next").click(function() {
-    $(current).hide();
+    $("#" + current).addClass("hidden");
     counter += 1;
-    $(current).show();
+    current = "card" + counter;
+    $("#" + current).removeClass("hidden");
   });
+
+// Previous Button
+  $("#prev").click(function() {
+    $("#" + current).addClass("hidden");
+    counter -= 1;
+    current = "card" + counter;
+    $("#" + current).removeClass("hidden");
+  });
+
 });
