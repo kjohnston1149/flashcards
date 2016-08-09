5569476a6d11512bac4b4cb2053d047120845cef
Kevin Johnston and Mark Lawson <kevin@>
2016-08-09 11:20:24 -0700

    add new cards and a random button feature

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,11 @@ $(function() {
 // Next Button
   $("#next").click(function() {
     $("#" + current).addClass("hidden");
-    counter += 1;
+    if (counter = 19) {
+      counter = 1;
+    } else {
+      counter += 1;
+    }
     current = "card" + counter;
     $("p").hide();
     $("ul").hide();
@@ -21,7 +25,21 @@ $(function() {
 // Previous Button
   $("#prev").click(function() {
     $("#" + current).addClass("hidden");
-    counter -= 1;
+    if (counter = 1) {
+      counter = 19;
+    } else {
+      counter -= 1;
+    }
+    current = "card" + counter;
+    $("p").hide();
+    $("ul").hide();
+    $("#" + current).removeClass("hidden");
+  });
+
+// Random Button
+  $("#random").click(function() {
+    $("#" + current).addClass("hidden");
+    counter = Math.floor(Math.random()*19)+1;
     current = "card" + counter;
     $("p").hide();
     $("ul").hide();
